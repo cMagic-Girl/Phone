7951d22fa5a38c66b71c41124b404f5acd096547
咕咕 <baiguguos@gmail.com>
2025-12-03 00:37:37 +0800

    删除 多余的手机Menu/Screen

--- src/main/java/baios/magicgirl/phone/screen/ModScreens.java
+++ src/main/java/baios/magicgirl/phone/screen/ModScreens.java
@@ -1,7 +1,6 @@
 package baios.magicgirl.phone.screen;
 
 import baios.magicgirl.phone.menu.ModMenus;
-import baios.magicgirl.phone.menu.PhoneMainMenu;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -12,7 +11,6 @@ public class ModScreens {
     @SubscribeEvent
     public static void clientLoad(RegisterMenuScreensEvent event) {
         event.register(ModMenus.PHONE_MENU.get(), PhoneScreen::new);
-        event.register(ModMenus.PHONE_MAIN_MENU.get(), PhoneMainScreen::new);
     }
 
     public interface ScreenAccessor {
