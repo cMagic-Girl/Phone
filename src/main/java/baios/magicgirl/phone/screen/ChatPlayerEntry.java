ddf0aed661f082fadd65f5a0dbbb6ffa7924489f
咕咕 <baiguguos@gmail.com>
2025-12-07 12:30:52 +0800

    新增 聊天列表显示角色名

--- src/main/java/baios/magicgirl/phone/screen/ChatPlayerEntry.java
+++ src/main/java/baios/magicgirl/phone/screen/ChatPlayerEntry.java
@@ -1,11 +1,14 @@
 package baios.magicgirl.phone.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -16,6 +19,7 @@ import java.util.function.Consumer;
 public class ChatPlayerEntry extends ContainerObjectSelectionList.Entry<ChatPlayerEntry> {
     private final ResourceLocation playerAvatar;
     private final String playerName;
+    private final Font font= Minecraft.getInstance().font;
 
     // 视觉状态
     private boolean isHovered;
@@ -26,15 +30,18 @@ public class ChatPlayerEntry extends ContainerObjectSelectionList.Entry<ChatPlay
     public ChatPlayerEntry(ResourceLocation playerAvatar, String name, Consumer<ChatPlayerEntry> onClickCallback) {
         super();
         this.playerAvatar = playerAvatar;
-        this.playerName = name;
+        this.playerName = I18n.get("gui.magic_girl_phone."+ name);
         this.onClickCallback = onClickCallback;
     }
+
+
 
     @Override
     public void render(GuiGraphics guiGraphics, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovered, float partialTick) {
         this.isHovered = hovered;
         renderBackground(guiGraphics, left, top, width, height);
         RenderSystem.enableBlend(); // 透明头像适配
+        guiGraphics.drawString(this.font, playerName, left + 85, top + 3, 0xFFFFFF);
         guiGraphics.blit(this.playerAvatar, left + 55, top + 1, 0, 0, 28, 28, 28, 28);
         RenderSystem.disableBlend();
     }
